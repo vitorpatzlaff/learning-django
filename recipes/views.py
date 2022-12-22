38e15c0d022e0ccabee4a08ba087eb5cd4594aec
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-22 19:23:02 -0300

    remove contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,9 @@
 # Create your views here.
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
-
-
-def contact(request):
-    return HttpResponse('contato')
-
-
-def about(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html', status=200, context={
+        'name': 'Vitor Hugo'
+    })

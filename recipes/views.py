d591250c781400c2447719d10db55661ddc4db00
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-24 12:03:40 -0300

    add faker to fill recipes data

--- recipes/views.py
+++ recipes/views.py
@@ -2,14 +2,16 @@
 
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', status=200, context={
-        'name': 'Vitor Hugo'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'name': 'Vitor Hugo'
+        'recipe': make_recipe()
     })

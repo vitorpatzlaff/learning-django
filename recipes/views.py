fa2826c885f21d3450ed7efd42b3e0a6ddb7c0ec
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-25 13:02:30 -0300

    allow html on recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+
+# from utils.recipes.factory import make_recipe
 
 
 def home(request):
@@ -14,14 +15,22 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', status=200, context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Categoria | '
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id, is_published=True
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })

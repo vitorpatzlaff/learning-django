e3a184c441b93319a7ede960e349c8bce1822faa
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-25 01:43:00 -0300

    add category.html

--- recipes/views.py
+++ recipes/views.py
@@ -7,15 +7,15 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', status=200, context={
         'recipes': recipes
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', status=200, context={
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', status=200, context={
         'recipes': recipes
     })
 

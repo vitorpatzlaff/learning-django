ac3a9f7ccf46cb373ee74110f986eb90f5165fbe
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-28 14:58:31 -0300

    split views tests on different files

---
+++ recipes/tests/test_recipe_category_view.py
@@ -0,0 +1,26 @@
+from django.urls import resolve, reverse
+
+from recipes import views
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeCategoryViewTest(RecipeTestBase):
+    def test_recipe_category_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        self.assertEqual(response.status_code, 404)
---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,42 @@
+from django.urls import resolve, reverse
+
+from recipes import views
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeHomeViewTest(RecipeTestBase):
+    def test_recipe_home_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_load_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('No recipes found here 😥', response.content.decode('utf-8'))
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn('No recipes found here 😥', response.content.decode('utf-8'))

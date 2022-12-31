8beb50a774ad6e289b9ab0ceacbe18a622aad65f
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-12-31 18:24:53 -0300

    add logout system

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -31,6 +32,7 @@ def register_create(request):
         messages.success(request, 'Your user is created, please log in.')
 
         del request.session['register_form_data']
+        return redirect('authors:login')
 
     return redirect('authors:register')
 
@@ -65,3 +67,11 @@ def login_create(request):
         messages.error(request, 'Invalid username or password')
 
     return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if request.POST and request.POST.get('username') == request.user.username:
+        logout(request)
+
+    return redirect(reverse('authors:login'))

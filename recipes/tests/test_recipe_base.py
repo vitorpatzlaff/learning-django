76b1c6912ea95a33473e76fbe5abd0bfef4eac42
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-02 17:22:41 -0300

    add mixin class

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -3,13 +3,7 @@ from django.test import TestCase
 from recipes.models import Category, Recipe, User
 
 
-class RecipeTestBase(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        return super().tearDown()
-
+class RecipeMixin:
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
 
@@ -61,3 +55,8 @@ class RecipeTestBase(TestCase):
             preparation_steps_is_html=preparation_steps_is_html,
             is_published=is_published,
         )
+
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()

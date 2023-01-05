b72e12b620ecc58fe14359928367c0a5b0099965
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-04 22:13:35 -0300

    add recipe detail view

--- recipes/views.py
+++ recipes/views.py
@@ -107,3 +107,18 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True
     })
+
+
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        ctx.update({
+            'is_detail_page': True
+        })
+
+        return ctx

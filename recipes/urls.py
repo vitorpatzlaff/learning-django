ddcc7d53833527bb489dfd3a834ddba5fbaf6198
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-04 21:00:03 -0300

    add recipe class views

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,8 +5,12 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('recipes/search/', views.search, name='search'),
-    path('recipes/category/<int:category_id>/', views.category, name='category'),
+    path('', views.RecipeListViewBase.as_view(), name='home'),
+    path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(),
+        name='category'
+    ),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]

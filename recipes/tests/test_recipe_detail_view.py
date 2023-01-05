7b3f809f89bc7f9a3970a5d9cfcd051cca13b24d
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-05 00:39:36 -0300

    fix views and tests

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -7,11 +7,11 @@ from .test_recipe_base import RecipeTestBase
 
 class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_view_functions_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
+        view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': 1000}))
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_template_loads_correct_recipe(self):
@@ -19,13 +19,13 @@ class RecipeDetailViewTest(RecipeTestBase):
 
         self.make_recipe(title=needed_title)
 
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': 1}))
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
 
     def test_recipe_detail_template_dont_load_recipe_not_published(self):
         recipe = self.make_recipe(is_published=False)
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.id}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': recipe.id}))
 
         self.assertEqual(response.status_code, 404)

8f005dad97db116409cc8848caec5e4991296ead
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-09 17:25:47 -0300

    add Tag to django admin area

--- tag/models.py
+++ tag/models.py
@@ -13,7 +13,7 @@ class Tag(models.Model):
 
     # Fields for generic relations
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.CharField()
+    object_id = models.CharField(max_length=255)
 
     content_object = GenericForeignKey('content_type', 'object_id')
 

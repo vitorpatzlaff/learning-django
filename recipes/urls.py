fc7b0250ca6bf535db34117f427775936cf87b67
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-09 18:25:33 -0300

    add tag url, view, and template

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,14 +6,31 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.RecipeListViewBase.as_view(), name='home'),
-    path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(),
+        name='search'
+    ),
+    path(
+        'recipes/tags/<slug:slug>',
+        views.RecipeListViewTag.as_view(),
+        name='tag'
+    ),
     path(
         'recipes/category/<int:category_id>/',
         views.RecipeListViewCategory.as_view(),
         name='category'
     ),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
-    path('recipes/api/v1/', views.RecipeListViewHomeAPI.as_view(), name='recipe_api_v1'),
+    path(
+        'recipes/<int:pk>/',
+        views.RecipeDetail.as_view(),
+        name='recipe'
+    ),
+    path(
+        'recipes/api/v1/',
+        views.RecipeListViewHomeAPI.as_view(),
+        name='recipe_api_v1'
+    ),
     path(
         'recipes/api/v1/<int:pk>/',
         views.RecipeDetailAPI.as_view(),
--- recipes/views.py
+++ recipes/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 # from utils.recipes.factory import make_recipe
@@ -45,6 +46,7 @@ class RecipeListViewBase(ListView):
             is_published=True
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -93,6 +95,31 @@ class RecipeListViewCategory(RecipeListViewBase):
 
         ctx.update({
             'title': f'{ctx.get("recipes")[0].category.name} - Categoria | '
+        })
+
+        return ctx
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': page_title,
         })
 
         return ctx

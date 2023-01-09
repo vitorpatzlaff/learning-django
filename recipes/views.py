cf0dca9afd90f8ce240aa8a9c47aa3550d0f5d70
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-09 15:01:44 -0300

    use aggregate

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db.models import Q
+from django.db.models.aggregates import Count
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
@@ -16,11 +17,12 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects \
-        .defer('is_published')
+    recipes = Recipe.objects.values('id', 'title')[:15]
+    number_of_recipes = recipes.aggregate(number=Count('id'))
 
     context = {
-        'recipes': recipes
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number'],
     }
 
     return render(

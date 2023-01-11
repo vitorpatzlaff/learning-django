be9510fb1a8ebdb30d2150b7fac8c87957402d8e
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-11 18:42:30 -0300

    add more fields

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,11 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField(method_name='any_method_name')
+
+    # def get_preparation(self, recipe):
+    #     return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'

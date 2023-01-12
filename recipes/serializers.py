6b562911c16b27a66996c6e6713c942d6f4011ba
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-11 21:18:17 -0300

    use ModelSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,39 +1,32 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from tag.models import Tag
 
-from .models import Category
+from .models import Recipe
 
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
+
+    # id = serializers.IntegerField()
+    # name = serializers.CharField(max_length=255)
+    # slug = serializers.SlugField()
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField(method_name='any_method_name')
-    category = serializers.PrimaryKeyRelatedField(
-        queryset=Category.objects.all()
-    )
-    category_name = serializers.StringRelatedField(source='category')
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
-    author_name = serializers.StringRelatedField(source='author')
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True,
-    )
-    tag_objects = TagSerializer(
-        many=True,
-        source='tags'
-    )
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author', 'category', 'tags', 'public', 'preparation',
+            'tag_objects', 'tag_links'
+        ]
+
+    public = serializers.BooleanField(source='is_published', read_only=True)
+    preparation = serializers.SerializerMethodField(method_name='any_method_name', read_only=True)
+    category = serializers.StringRelatedField(read_only=True)
+    tag_objects = TagSerializer(many=True, source='tags', read_only=True)
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',

148b07d58c5e35a42defa78412b9e3113c42b7cf
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-12 22:59:07 -0300

    add SimpleRouter

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
 from recipes import views
 
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register('recipes/api/v2', views.RecipeAPIv2ViewSet, basename='recipes-api')
 
 urlpatterns = [
     path('', views.RecipeListViewBase.as_view(), name='home'),
@@ -41,26 +45,29 @@ urlpatterns = [
         views.theory,
         name='theory',
     ),
-    path(
-        'recipes/api/v2/',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'list',
-            'post': 'create',
-        }),
-        name='recipe_api_v2',
-    ),
-    path(
-        'recipes/api/v2/<int:pk>',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'retrieve',
-            'patch': 'partial_update',
-            'delete': 'destroy',
-        }),
-        name='recipe_api_v2_detail',
-    ),
+    # path(
+    #     'recipes/api/v2/',
+    #     views.RecipeAPIv2ViewSet.as_view({
+    #         'get': 'list',
+    #         'post': 'create',
+    #     }),
+    #     name='recipe_api_v2',
+    # ),
+    # path(
+    #     'recipes/api/v2/<int:pk>',
+    #     views.RecipeAPIv2ViewSet.as_view({
+    #         'get': 'retrieve',
+    #         'patch': 'partial_update',
+    #         'delete': 'destroy',
+    #     }),
+    #     name='recipe_api_v2_detail',
+    # ),
     path(
         'recipes/api/v2/tag/<int:pk>',
         views.tag_api_detail,
         name='recipe_api_v2_tag',
     ),
+    path('', include(recipe_api_v2_router.urls))
 ]
+
+# urlpatterns += recipe_api_v2_router.urls

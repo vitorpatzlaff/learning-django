8c48fe8a57238a6304d8b663bc8edbc5f819a147
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-14 15:41:51 -0300

    add not_published test

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -7,18 +7,30 @@ from recipes.tests.test_recipe_base import RecipeMixin
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
+    def get_recipe_api_list(self, query_string=''):
+        api_url = reverse('recipes:recipes-api-list') + query_string
+        return self.client.get(api_url)
+
     def test_recipe_api_list_returns_status_code_200(self):
-        api_url = reverse('recipes:recipes-api-list')
-        response = self.client.get(api_url)
+        response = self.get_recipe_api_list()
         self.assertEqual(response.status_code, 200)
 
     @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
     def test_recipe_api_list_laods_correct_number_of_recipes(self):
         wanted_number_of_recipes = 7
         self.make_recipe_in_batch(qtd=wanted_number_of_recipes)
-        response = self.client.get(reverse('recipes:recipes-api-list'))
+        response = self.get_recipe_api_list()
         qtd_of_loaded_recipes = len(response.data.get('results'))
         self.assertEqual(
             wanted_number_of_recipes,
             qtd_of_loaded_recipes
         )
+
+    def test_recipe_api_list_do_not_show_not_published_recipes(self):
+        recipes = self.make_recipe_in_batch(qtd=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+        response = self.get_recipe_api_list()
+
+        self.assertEqual(len(response.data.get('results')), 1)

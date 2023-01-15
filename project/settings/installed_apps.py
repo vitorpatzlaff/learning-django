a8a4e505a27396dbd69ab87aba7f162f6fb10abb
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-01-14 21:11:28 -0300

    add cors configs

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,10 +1,12 @@
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # CORS Headers
+    'corsheaders',
     # Django rest framework
     'rest_framework_simplejwt',
     'rest_framework',
